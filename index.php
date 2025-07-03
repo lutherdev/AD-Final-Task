5ee0c859fca011e1ec10649c61da41af5fee1d49
sijeeee <ceejjay73@gmail.com>
2025-07-03 22:50:04 +0800

    feat (index.php): added tester

--- index.php
+++ index.php
@@ -8,10 +8,11 @@ require_once BASE_PATH . '/bootstrap.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="assets/css/nav.component.css">
 </head>
 <body>
     <?php 
-    
+    require_once 'components/templates/nav.component.php';
     include_once HANDLERS_PATH . "/postgreTester.handler.php";
 ?>
 </body>
