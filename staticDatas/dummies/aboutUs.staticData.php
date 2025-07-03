9a337c011dd0bbaf0635ab63f12b96301e96ecd2
sijeeee <ceejjay73@gmail.com>
2025-07-04 00:42:28 +0800

    feat (About us): added dummy data for testing

---
+++ staticDatas/dummies/aboutUs.staticData.php
@@ -0,0 +1,18 @@
+<?php
+
+$team_members = [
+    ['name' => 'Luther Dean',
+    'role' => 'Master Digger',
+    'image' => '/img/member1.jpg',
+],
+    ['name' => 'Ping Wei',
+    'role' => 'Gold Digger',
+    'image' => '/img/member2.jpg',
+],
+    ['name' => 'Christian Molina',
+    'role' => 'Noob Digger',
+    'image' => '/img/member3.jpg',
+]
+    
+];
+?>
