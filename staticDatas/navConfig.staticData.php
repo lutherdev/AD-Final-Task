0beaaa7bd7f7e8bf805f2cf81c075db171b5cb0c
sijeeee <ceejjay73@gmail.com>
2025-07-04 00:46:32 +0800

    feat (navConfig.staticData.php): added static data

---
+++ staticDatas/navConfig.staticData.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    'customer' => [
+        'Home' => 'home.php',
+        'Shop' => 'shop.php',
+        'Contact Us' => 'contact.php',
+        'Logout' => 'logout.php'
+    ],
+    'admin' => [
+        'Dashboard' => 'admin/dashboard.php',
+        'Manage Users' => 'admin/users.php',
+        'Reports' => 'admin/reports.php',
+        'Logout' => 'logout.php'
+    ]
+];
