3926fcad53ad1e936a6488ec9c4db2eb92a7a654
lutherdev <devturd@gmail.com>
2025-07-04 18:13:28 +0800

    refactor (landing page): replaced contact us with the default home

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $uri = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), "/");
 
 // Default page
 if ($uri === "") {
-    $folder = "contactUs";
+    $folder = "home";
 } else {
     $folder = $uri;
 }
