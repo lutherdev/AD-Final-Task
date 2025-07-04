6edf56d740c4c7db227e4f43e0422824fb39a34a
lutherdev <devturd@gmail.com>
2025-07-04 23:37:29 +0800

    refactor (store): convert array to a static data dummy

--- pages/store/index.php
+++ pages/store/index.php
@@ -1,41 +1,5 @@
 <?php
-$products = [
-    [
-        'name' => 'Pickaxe',
-        'price' => 200,
-        'description' => 'Essential for mining stone and ore.',
-        'category' => 'Tools',
-        'image' => '/pages/store/assets/img/pickaxe.png'
-    ],
-    [
-        'name' => 'Gold Nugget',
-        'price' => 120,
-        'description' => 'Precious gold used for trading and decorations.',
-        'category' => 'Ore',
-        'image' => '/pages/store/assets/img/pickaxe.png'
-    ],
-    [
-        'name' => 'Iron Pickaxe',
-        'price' => 200,
-        'description' => 'Raw metal for crafting tools and armor.',
-        'category' => 'Tools',
-        'image' => '/pages/store/assets/img/pickaxe.png'
-    ],
-    [
-        'name' => 'Head Lamp',
-        'price' => 90,
-        'description' => 'Keep your tunnels lit and safe.',
-        'category' => 'Gear',
-        'image' => '/pages/store/assets/img/pickaxe.png'
-    ],
-    [
-        'name' => 'Miner\'s Helmet',
-        'price' => 150,
-        'description' => 'Protects your head from falling rocks.',
-        'category' => 'Gear',
-        'image' => '/pages/store/assets/img/pickaxe.png'
-    ],
-];
+require_once STATICDATAS_PATH . '/dummies/store.staticData.php';
 ?>
 
 <section class="store-container" id="inventory">
