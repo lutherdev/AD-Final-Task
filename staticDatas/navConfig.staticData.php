0d3f8eed28ffdc2a6551d721ec4328b15368f028
lutherdev <devturd@gmail.com>
2025-07-04 17:51:52 +0800

    refactor (navConfig): replaced values with correct ones for the use of url parse and trim

--- staticDatas/navConfig.staticData.php
+++ staticDatas/navConfig.staticData.php
@@ -4,16 +4,16 @@ require_once BASE_PATH . '/bootstrap.php';
 
 return [
     'customer' => [
-        'Home' => 'home.php',
-        'About Us' => PAGES_PATH . '/aboutUs/index.php',
-        'Shop' => 'shop.php',
-        'Contact Us' => PAGES_PATH . '/contactUs/index.php',
-        'Logout' => 'logout.php'
+        'Home' => '',
+        'About Us' => 'aboutUs',
+        'Shop' => 'shop',
+        'Contact Us' => 'contactUs',
+        'Logout' => 'logout'
     ],
     'admin' => [
-        'Dashboard' => 'admin/dashboard.php',
-        'Manage Users' => 'admin/users.php',
-        'Reports' => 'admin/reports.php',
-        'Logout' => 'logout.php'
+        'Dashboard' => 'dashboard',
+        'Manage Users' => 'manageUsers',
+        'Reports' => 'reports',
+        'Logout' => 'logout'
     ]
 ];
