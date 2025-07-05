fc8c1d1e27241088892abed8b58d5c8b2dd33d91
lutherdev <devturd@gmail.com>
2025-07-05 19:34:23 +0800

    fix (auth handler): removed the echo so header can run properly

--- utils/auth.util.php
+++ utils/auth.util.php
@@ -4,17 +4,10 @@ class Auth{
     public static function init(): void {
     if (session_status() === PHP_SESSION_NONE) { //IF SESSION IS NONE : LIKE NO STARTED SESSION THEN =
         session_start(); // START THE SESSION NOW!
-    } 
-
-    if (isset($_SESSION['user'])) {
-        echo "You are now logged in as " . $_SESSION['user']['first_name'] . '<br>';
-        echo "Your role is : " . $_SESSION['user']['role'] . '<br>';
-        echo 'Your money is : ' . $_SESSION['user']['wallet'];
-    } else {
-        echo "No user is logged in.";
-    }
-}
+        } 
+    }   
     //FOR LOGGIN IN
+
     //TODO: CREATE A TRY CATCH FUNCTION FOR FETCHING A ROW, ALSO A DEBUG IF WE GOT A ROW? AND A PASSWWORD VERIFY
     public static function attempt(PDO $pdo, string $username, string $password): bool {
         $stmt = $pdo->prepare("SELECT * FROM users WHERE username = :username");
