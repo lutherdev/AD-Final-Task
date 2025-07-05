1677ea0501c5da5a8e24c5a837b2d51f8460d63d
pingweiii <pwchen31@gmail.com>
2025-07-06 01:42:38 +0800

    feat(remove items): added simple layout for removing items

---
+++ pages/remove-items/index.php
@@ -0,0 +1,12 @@
+<div class="remove">
+    <h2>Remove Item</h2>
+    <form action="handlers/remove-item.handler.php" method="POST">
+        <label>ID:</label>
+        <input type="text" name="id" required>
+
+        <label>Item Name:</label>
+        <input type="text" name="item-name">
+
+        <button type="submit">Add Item</button>
+    </form>
+  </div>
