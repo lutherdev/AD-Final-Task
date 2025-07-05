39502814ed843481fe0f92c7154857b387d2981c
pingweiii <pwchen31@gmail.com>
2025-07-05 13:43:35 +0800

    refactor(contact us): changed the image in location

--- pages/contactUs/index.php
+++ pages/contactUs/index.php
@@ -1,39 +1,40 @@
-
 <main class="contactus-container">
 
     <div class="location">
-        <h1>Contact Details</h1>
-        <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3023.3120876980916!2d-74.03869172366096!3d40.769225035652654!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c2f66333a72a37%3A0x55829c31b37844e3!2s1209%20New%20York%20Ave%2C%20Union%20City%2C%20NJ%2007087%2C%20USA!5e0!3m2!1sen!2sph!4v1719999999999"
-        allowfullscreen="" loading="lazy"></iframe>
+        <h1>Summon the Minekeeper</h1>
+
+        <div class="medieval-map">
+                <img src="/pages/contactUs/assets/img/mineforgeMAP.png" alt="Mine Location Map">
+            </a>
+            <p class="map-hint">[Tap the map to open modern directions]</p>
+        </div>
 
         <div class="contact-info">
-            <p><span class="label">Address: </span>333 Sampaloc Manila</p>
-            <p><span class="label">Phone: </span>+63 987 654 3210</p>
-            <p><span class="label">Email: </span>mineforge333@gmail.com</p>
+            <p><span class="label">Coordinates of the Lodge: </span>333 Sampaloc, Kingdom of Manila</p>
+            <p><span class="label">Whispering Horn: </span>+63 987 654 3210</p>
+            <p><span class="label">Pigeon Scroll: </span>mineforge333@gmail.com</p>
         </div>
     </div>
 
-
     <div class="comment">
-        <h1>Have a Questions?</h1>
+        <h1>Send a Raven</h1>
         <form onsubmit="return false;">
             <div class="box-name">
-                <input type="text" name="name" placeholder="Enter Your Name" required>
+                <input type="text" name="name" placeholder="Enter thy Name" required>
             </div>
 
             <div class="box-email">
-                <input type="email" name="email" placeholder="Enter Your Email" required>
+                <input type="email" name="email" placeholder="Enter thy Scroll Address" required>
             </div>
 
             <div class="box-message">
-                <textarea name="message" placeholder="Enter Your Message" required></textarea>
+                <textarea name="message" placeholder="State thy Message for the Forge" required></textarea>
             </div>
 
             <div class="box-button">
-                <button type="submit">Send your Message</button>
+                <button type="submit">Dispatch Scroll</button>
             </div>
-
         </form>
     </div>
+
 </main>
