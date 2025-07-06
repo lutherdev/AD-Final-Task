368540e5bf31471a3be86483459eb5a7b4869d26
pingweiii <pwchen31@gmail.com>
2025-07-06 14:32:38 +0800

    feat(store.staticData.php): added more items in the menu

--- staticDatas/dummies/store.staticData.php
+++ staticDatas/dummies/store.staticData.php
@@ -35,4 +35,60 @@ $products = [
         'category' => 'Gear',
         'image' => '/pages/store/assets/img/pickaxe.png'
     ],
+    [
+        'name' => 'Steel Shovel',
+        'price' => 120,
+        'description' => 'Essential for digging through dirt and gravel.',
+        'category' => 'Tools',
+        'image' => '/pages/store/assets/img/shovel.png'
+    ],
+    [
+        'name' => 'Ore Cart',
+        'price' => 300,
+        'description' => 'Used to transport large amounts of ore.',
+        'category' => 'Gear',
+        'image' => '/pages/store/assets/img/cart.png'
+    ],
+    [
+        'name' => 'Mining Drill',
+        'price' => 500,
+        'description' => 'Powered tool for deep rock drilling.',
+        'category' => 'Tools',
+        'image' => '/pages/store/assets/img/drill.png'
+    ],
+    [
+        'name' => 'Dynamite Stick',
+        'price' => 250,
+        'description' => 'Used to blast through tough rock formations.',
+        'category' => 'Tools',
+        'image' => '/pages/store/assets/img/dynamite.png'
+    ],
+    [
+        'name' => 'Protective Gloves',
+        'price' => 80,
+        'description' => 'Guards your hands from blisters and sharp edges.',
+        'category' => 'Gear',
+        'image' => '/pages/store/assets/img/gloves.png'
+    ],
+    [
+        'name' => 'Mining Boots',
+        'price' => 130,
+        'description' => 'Durable boots with reinforced soles.',
+        'category' => 'Gear',
+        'image' => '/pages/store/assets/img/boots.png'
+    ],
+    [
+        'name' => 'Lantern',
+        'price' => 70,
+        'description' => 'Reliable source of light underground.',
+        'category' => 'Gear',
+        'image' => '/pages/store/assets/img/lantern.png'
+    ],
+    [
+        'name' => 'Gold Pan',
+        'price' => 60,
+        'description' => 'Used for panning gold in riverbeds.',
+        'category' => 'Tools',
+        'image' => '/pages/store/assets/img/goldpan.png'
+    ]
 ];
