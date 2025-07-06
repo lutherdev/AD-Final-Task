887db8f16ef92c94f66f91d9ae4b440834ce891c
pingweiii <pwchen31@gmail.com>
2025-07-06 15:15:03 +0800

    feat(manage-items): added simple layout for manage-items

---
+++ pages/manage-items/index.php
@@ -0,0 +1,9 @@
+<div class="manage-outer-container">
+  <div class="manage-container">
+    <h1>🛡️ Manage Your Items 🛡️</h1>
+
+    <a class="manage-button" href="add-items">Add Items</a>
+    <a class="manage-button" href="edit-items">Edit Items</a>
+    <a class="manage-button" href="remove-items">Remove Items</a>
+  </div>
+</div>
